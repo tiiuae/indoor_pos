42dc5c715c49852c50ef9696d3b630d5825ca02f
Jari Nippula <jarix@ssrc.tii.ae>
2021-02-16 14:50:55 +0200

    North offset given in degrees

--- src/indoor_pos.cpp
+++ src/indoor_pos.cpp
@@ -14,6 +14,7 @@ Convert indoor positioning data from lighthouse to PX4 positioning sensor data
 
 //#define INDOOR_USE_SIMULATOR
 
+const double Pi = 3.141592654;
 const uint64_t MaxRttSample = 1000000;
 
 using std::placeholders::_1;
@@ -60,12 +61,14 @@ IndoorPos::IndoorPos()
     this->declare_parameter<int>("frequency", 10);
     this->declare_parameter<double>("north_offset", 0.0);
 
+    double n_off = 0.0;
     auto point = geographic_msgs::msg::GeoPoint();
     this->get_parameter("home_lat", point.latitude);
     this->get_parameter("home_lon", point.longitude);
     this->get_parameter("home_alt", point.altitude);
     this->get_parameter("frequency", _impl->_update_freq);
-    this->get_parameter("north_offset", _impl->_north_offset);
+    this->get_parameter("north_offset", n_off);
+    _impl->_north_offset = (n_off / 360.0) * 2 * Pi;
 
     RCLCPP_INFO(this->get_logger(), "Home coordinates: lat: %lf, lon: %lf, alt: %lf",
         point.latitude, point.longitude, point.altitude);
@@ -79,6 +82,8 @@ IndoorPos::IndoorPos()
         _impl->_home.zone,
         _impl->_home.band
         );
+    RCLCPP_INFO(this->get_logger(), "Update freq: %d Hz, north_offset: %.3lf deg (%lf rad)",
+        _impl->_update_freq, n_off, _impl->_north_offset);
 
     _impl->_timesync_sub = this->create_subscription<px4_msgs::msg::Timesync>(
         "Timesync_PubSubTopic", 10, std::bind(&IndoorPos::TimeSync, this, _1));
